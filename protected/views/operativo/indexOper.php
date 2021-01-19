85e9835858b81e333f75f62485342d7cba75b213
Cristhian Moya <crmoya@gmail.com>
2021-01-19 10:33:43 -0300

    registo cambiado por registro

--- protected/views/operativo/indexOper.php
+++ protected/views/operativo/indexOper.php
@@ -1,15 +1,15 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
 Bienvenido <?php echo CHtml::encode($nombre);?>, por favor seleccione una de las siguientes operaciones para comenzar:<br/><br/>
 <ul>
- <li><?php echo CHtml::link("Registo Control de Maquinarias para camiones, camionetas, autos Propios",CController::createUrl('//operativo/camionesPropios')); ?></li>
- <li><?php echo CHtml::link("Registo Control de Maquinarias para camiones, camionetas, autos Arrendados",CController::createUrl('//operativo/camionesArrendados')); ?></li>
- <li><?php echo CHtml::link("Registo Control de Maquinarias para Equipos Propios",CController::createUrl('//operativo/equiposPropios')); ?></li>
- <li><?php echo CHtml::link("Registo Control de Maquinarias para Equipos Arrendados",CController::createUrl('//operativo/equiposArrendados')); ?></li>
+ <li><?php echo CHtml::link("Registro Control de Maquinarias para camiones, camionetas, autos Propios",CController::createUrl('//operativo/camionesPropios')); ?></li>
+ <li><?php echo CHtml::link("Registro Control de Maquinarias para camiones, camionetas, autos Arrendados",CController::createUrl('//operativo/camionesArrendados')); ?></li>
+ <li><?php echo CHtml::link("Registro Control de Maquinarias para Equipos Propios",CController::createUrl('//operativo/equiposPropios')); ?></li>
+ <li><?php echo CHtml::link("Registro Control de Maquinarias para Equipos Arrendados",CController::createUrl('//operativo/equiposArrendados')); ?></li>
  <li></li>
- <li><?php echo CHtml::link("Modificar Registo Control de Maquinarias para camiones, camionetas, autos Propios",CController::createUrl('//rCamionPropio/admin2')); ?></li>
- <li><?php echo CHtml::link("Modificar Registo Control de Maquinarias para camiones, camionetas, autos Arrendados",CController::createUrl('//rCamionArrendado/admin2')); ?></li>
- <li><?php echo CHtml::link("Modificar Registo Control de Maquinarias para Equipos Propios",CController::createUrl('//rEquipoPropio/admin2')); ?></li>
- <li><?php echo CHtml::link("Modificar Registo Control de Maquinarias para Equipos Arrendados",CController::createUrl('//rEquipoArrendado/admin2')); ?></li>
+ <li><?php echo CHtml::link("Modificar Registro Control de Maquinarias para camiones, camionetas, autos Propios",CController::createUrl('//rCamionPropio/admin2')); ?></li>
+ <li><?php echo CHtml::link("Modificar Registro Control de Maquinarias para camiones, camionetas, autos Arrendados",CController::createUrl('//rCamionArrendado/admin2')); ?></li>
+ <li><?php echo CHtml::link("Modificar Registro Control de Maquinarias para Equipos Propios",CController::createUrl('//rEquipoPropio/admin2')); ?></li>
+ <li><?php echo CHtml::link("Modificar Registro Control de Maquinarias para Equipos Arrendados",CController::createUrl('//rEquipoArrendado/admin2')); ?></li>
  <li></li>
  <li><?php echo CHtml::link("Ingresar camiones, camionetas, autos Propios",CController::createUrl('//camionPropio/create')); ?></li>
  <li><?php echo CHtml::link("Ingresar camiones, camionetas, autos Arrendados",CController::createUrl('//camionArrendado/create')); ?></li>
