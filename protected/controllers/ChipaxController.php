05b260b7d70f846cc6e9ef4e131db1916dee9a11
Cristhian Moya <cristhian.moya.b@outlook.com>
2021-10-21 22:48:45 -0300

    agregado hash

--- protected/controllers/ChipaxController.php
+++ protected/controllers/ChipaxController.php
@@ -1620,6 +1620,19 @@ class ChipaxController extends Controller
     public function actionGetFaenas(){
         ini_set("memory_limit", "-1");
         set_time_limit(0);
+
+        $hash = Yii::app()->request->getQuery('hash');
+        $local_hash = Tools::chipaxSecret(0);
+        $intentos = 1;
+        while ($local_hash != $hash && $intentos <= 30) {
+            $local_hash = Tools::chipaxSecret($intentos);
+            $intentos++;
+        }
+        if ($hash != $local_hash) {
+            throw new Exception("Hash incorrecto");
+        }
+
+        
         $respuesta = json_encode(['status'=>'ERROR']);
         try {
             $categoriaJson = json_decode(file_get_contents("php://input"), true);
