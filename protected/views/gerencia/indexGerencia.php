240b85ca3ff8e8435643fa4f7e9390c478652368
leoj2eephp <leandro.j2ee@gmail.com>
2022-08-17 22:00:45 -0400

    New! Se agrega enlace para probar nueva funcionalidad de remuneraciones.

--- protected/views/gerencia/indexGerencia.php
+++ protected/views/gerencia/indexGerencia.php
@@ -8,6 +8,7 @@ Bienvenido <?php echo CHtml::encode($nombre);?>, por favor seleccione una de las
  <li><?php echo CHtml::link("Informe de Gasto de Combustible",CController::createUrl('//gastoCombustible/admin')); ?></li>
  <li><?php echo CHtml::link("Informe de Gasto de Repuestos",CController::createUrl('//gastoRepuesto/admin')); ?></li>
  <li><?php echo CHtml::link("Informe de Gasto de Remuneraciones",CController::createUrl('//gastoRemuneraciones/admin')); ?></li>
+ <li><?php echo CHtml::link("(Nuevo) Informe de Gasto de Remuneraciones",CController::createUrl('//remuneraciones-sam/index')); ?></li>
 </ul>
 <ul>
  <li><?php echo CHtml::link("Informe de Resultados",CController::createUrl('//resultados/admin')); ?></li>
