4e3da940e54de31178e720990efe22306c552a6c
leoj2eephp <leandro.j2ee@gmail.com>
2022-09-14 20:08:42 -0300

    Tropezando con la misma piedra 2 veces!!

--- protected/controllers/ExpedicionesEquipoPropioController.php
+++ protected/controllers/ExpedicionesEquipoPropioController.php
@@ -178,7 +178,7 @@ class ExpedicionesEquipoPropioController extends Controller {
 			$criteria->params[':fecha_inicio'] = $model->fecha_inicio;
 			$criteria->params[':fecha_fin'] = $model->fecha_fin;
 		}
-		$criteria->compare('camionPropio_id',$model->equipo_id);
+		$criteria->compare('equipoPropio_id',$model->equipo_id);
 		$criteria->addCondition("tipo_equipo_camion = :ep");
 		$criteria->params[":ep"] = "EP";
 		//$criteria->group = "";
